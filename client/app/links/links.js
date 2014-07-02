191653291c7e74ae97ff6986884f254d2822492f
lawmay <lmay01@gmail.com>
2014-07-02 16:35:41 -0700

    implemented linkscontrollers

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,19 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+
+  $scope.data = {};
+
+  $scope.getLinks = function() {
+    Links.getLinks()
+      .then(function(links) {
+        $scope.data.links = links;
+      })
+      .catch(function(error) {
+    });
+  };
+
+  // $scope.data2 = $scope.getLinks();
+  $scope.getLinks();
+
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,12 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+
+
+  $scope.link = {};
+
+  $scope.addLink = function(link) {
+
+  };
+
 });
